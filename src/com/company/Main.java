1ee95eaad0b7179f51192f6f26def8b1ac341e2f
Arkadiusz Pietrzycki <97041316+ArkadiuszPi@users.noreply.github.com>
2022-01-19 15:04:09 +0100

    Add to gitignore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First repo");// write your code here
+        System.out.println("First repo");
+        System.out.println("Without idea folder");// write your code here
     }
 }
